f986f24004a280f435510bf0032c1781ce1b17f6
dogumus35-blip <do.gumus35@gmail.com>
2026-08-09 10:29:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "🤖 Murcanlar botu aktif!\n\n"
-        "Bot başarıyla çalışıyor."
-    )
+async def mesaj(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_chat:
+        print("CHAT ID:", update.effective_chat.id)
+        print("CHAT TYPE:", update.effective_chat.type)
+        print("CHAT TITLE:", update.effective_chat.title)
 
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        MessageHandler(filters.ALL, mesaj)
+    )
 
     print("Bot çalışıyor...")
     app.run_polling()
