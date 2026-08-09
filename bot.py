2d0e7918a3081878c2797c4a8f1b26d0c5dad345
dogumus35-blip <do.gumus35@gmail.com>
2026-08-09 10:47:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,24 +4,19 @@ from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-
 async def mesaj(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat:
-        print("CHAT ID:", update.effective_chat.id)
+        print("CHAT ID:", update.effective_chat.id, "Message:", update.message.text)
         print("CHAT TYPE:", update.effective_chat.type)
         print("CHAT TITLE:", update.effective_chat.title)
 
-
 def main():
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(
         MessageHandler(filters.ALL, mesaj)
     )
-
     print("Bot çalışıyor...")
     app.run_polling()
 
-
-if __name__ == "__main__":
+if name == "__main__":
     main()
